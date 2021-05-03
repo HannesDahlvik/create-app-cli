79e9f7c15a557834baf986c5eae8b701a16e3e56
Hannes <hannes.dahlvik@gmail.com>
2021-05-03 19:29:08 +0300

    Added option to use npm or yarn to install packages

--- src/createApp.js
+++ src/createApp.js
@@ -1,7 +1,9 @@
 const inquirer = require('inquirer')
 const chalk = require('chalk')
+const Spinner = require('cli-spinner').Spinner
 const fse = require('fs-extra')
 const path = require('path')
+const ch = require('child_process')
 
 async function init() {
     const templates = fse.readdirSync(path.join(__dirname, '/templates'))
@@ -28,17 +30,31 @@ async function init() {
                 message: 'Select type of project',
                 name: 'type',
                 choices: typeChoices
+            },
+            {
+                type: 'list',
+                message: 'Select package manager',
+                name: 'manager',
+                choices: [{
+                        name: 'yarn',
+                        value: 'yarn'
+                    },
+                    {
+                        name: 'npm',
+                        value: 'npm'
+                    }
+                ]
             }
         ])
         .then((values) => {
-            createApp(values.name, values.type)
+            createApp(values.name, values.type, values.manager)
         })
         .catch((err) => {
             console.error(err)
         })
 }
 
-function createApp(name, type) {
+function createApp(name, type, manager) {
     const root = path.resolve(name)
 
     if (name !== '') fse.ensureDir(name)
@@ -46,19 +62,48 @@ function createApp(name, type) {
 
     if (type) {
         fse.copy(path.join(__dirname, `/templates/${type}/template`), root)
-            .then(() => {
-                console.log(`
-                
-    Made a new ${chalk.blueBright(`"${type}"`)} inside ${chalk.blueBright(`"${name}"`)}
-
-    Make sure to ${chalk.blueBright(`"cd ${name}"`)} and run ${chalk.blackBright(`"yarn"`)} or ${chalk.blueBright(`"npm install"`)}
-
-    Happy coding!
-                
-                `)
-            })
             .catch((err) => console.error(err))
     } else process.exit(1)
+
+    if (manager) {
+        const spinner = new Spinner(`Installing packages with ${chalk.cyan(`"${manager}"`)}`)
+        spinner.start()
+        if (manager === 'yarn') installWithYarn(root, type, name, spinner)
+        if (manager === 'npm') installWithNpm(root, type, name, spinner)
+    } else process.exit(1)
+}
+
+function installWithYarn(root, type, name, spinner) {
+    ch.exec('yarn', {
+            cwd: root
+        })
+        .addListener('close', () => {
+            logCreatedProject(type, name)
+            spinner.stop()
+        })
+}
+
+function installWithNpm(root, type, name, spinner) {
+    ch.exec('npm install', {
+            cwd: root
+        })
+        .addListener('close', () => {
+            logCreatedProject(type, name)
+            spinner.stop()
+        })
+}
+
+function logCreatedProject(type, name) {
+    console.log(`
+
+
+    Made a new ${chalk.cyan(`"${type}"`)} inside ${chalk.cyan(`"${name}"`)}
+
+    Make sure to ${chalk.cyan(`"cd ./${name}"`)} and
+
+    Happy coding!
+
+    `)
 }
 
 module.exports = {
