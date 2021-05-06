9278e5fee977df74ee4c12b2d2a2bd2522474f5a
Hannes <hannes.dahlvik@gmail.com>
2021-05-06 19:00:55 +0300

    Made defining package.json in template.json for templates

--- src/createApp.js
+++ src/createApp.js
@@ -5,18 +5,19 @@ const fse = require('fs-extra')
 const path = require('path')
 const ch = require('child_process')
 
+const templates = fse.readdirSync(path.join(__dirname, '/templates'))
+
 async function init() {
-    const templates = fse.readdirSync(path.join(__dirname, '/templates'))
     let typeChoices = []
     for (const templateName of templates) {
-        await fse.readJSON(path.join(__dirname, `/templates/${templateName}/template.json`))
+        fse.readJSON(path.join(__dirname, `/templates/${templateName}/template.json`))
             .then((templateJson) => {
                 typeChoices.push({
                     name: templateJson.name,
                     value: templateJson.value
                 })
             })
-            .catch((err) => console.error(err))
+            .catch((err) => exit(err))
     }
 
     inquirer
@@ -49,28 +50,48 @@ async function init() {
         .then((values) => {
             createApp(values.name, values.type, values.manager)
         })
-        .catch((err) => {
-            console.error(err)
-        })
+        .catch((err) => exit(err))
 }
 
 function createApp(name, type, manager) {
     const root = path.resolve(name)
 
     if (name !== '') fse.ensureDir(name)
-    else process.exit(1)
+    else exit()
 
     if (type) {
         fse.copy(path.join(__dirname, `/templates/${type}/template`), root)
-            .catch((err) => console.error(err))
-    } else process.exit(1)
-
-    if (manager) {
-        const spinner = new Spinner(`Installing packages with ${chalk.cyan(`"${manager}"`)}`)
-        spinner.start()
-        if (manager === 'yarn') installWithYarn(root, type, name, spinner)
-        if (manager === 'npm') installWithNpm(root, type, name, spinner)
-    } else process.exit(1)
+            .then(() => {
+                for (const templateName of templates) {
+                    fse.readJSON(path.join(__dirname, `/templates/${templateName}/template.json`))
+                        .then((templateJson) => {
+                            if (templateJson.value === type) {
+                                let packageJson = {
+                                    name: name,
+                                    version: '0.0.1',
+                                    license: 'MIT',
+                                    main: 'index.js',
+                                    ...templateJson.package
+                                }
+                                fse.writeJSON(`${root}/package.json`, packageJson, {
+                                        spaces: 4
+                                    })
+                                    .then(() => {
+                                        if (manager) {
+                                            const spinner = new Spinner(`Installing packages with ${chalk.cyan(`"${manager}"`)}`)
+                                            spinner.start()
+                                            if (manager === 'yarn') installWithYarn(root, type, name, spinner)
+                                            if (manager === 'npm') installWithNpm(root, type, name, spinner)
+                                        } else exit()
+                                    })
+                                    .catch((err) => exit(err))
+                            }
+                        })
+                        .catch(() => exit(err))
+                }
+            })
+            .catch((err) => exit(err))
+    } else exit()
 }
 
 function installWithYarn(root, type, name, spinner) {
@@ -104,6 +125,12 @@ function logCreatedProject(type, name) {
     Happy coding!
 
     `)
+}
+
+function exit(err) {
+    if (err) console.error(err)
+    console.log(chalk.redBright('Something went wrong!'));
+    process.exit(1)
 }
 
 module.exports = {
